f72237774bf3d2625cc6547d1b2bf6804a5ae26c
Seán McDonnell <seanmcdonnell90@gmail.com>
2015-02-16 22:26:54 +0000

    updated and cleaned index

--- index.php
+++ index.php
@@ -20,41 +20,7 @@ if($logged == true){
 
   <title>test</title>
 </head>
-<!-- NAVBAR
-  ================================================== -->
-  <body>
-
-    <h2 class="featurette-heading">Products Table Output<span class="text-muted">Queries in Progress</span></h2>
-    <?php
-
-    $output_table = 
-
-    '<table><tr><td><b>ID</b></td><td><b>Name</b></td><td><b>Description</b></td><td><b>Category</b></
-
-    td><td><b>Stock</b></td><td><b>Price</b></td><td><b>Org ID</b></td><td><b>Time 
-
-    Created</b></td></tr>';
-
-    $con = mysql_connect($mysql_host, $mysql_user, $mysql_password);
-    if (!$con) {
-      die('Could not connect: ' . mysql_error());
-    }
-    mysql_select_db($mysql_database, $con);
-
-    $result = mysql_query("SELECT * FROM testTable");
-    while($row = mysql_fetch_array($result))
-    {
-      $output_table .= '<tr><td><b>' . $row[0] . '</b></td><td>' . $row[1] . '</td><td>' 
-
-      . $row[2] . '</td><td>' . $row[3] . '</td><td>' . $row[4] . '</td><td>' . $row[5] . '</td><td>' . 
-
-      $row[6] . '</td><td>' . $row[7] . '</td><td>' . $row[8] . '</td><td>' . $row[9] . '</td></tr>';
-    }
-
-    $output_table .= '</table>';
-
-    echo $output_table;
-
-    ?>
-  </body>
-  </html>
+<body>
+<h2 class="featurette-heading">Products Table Output<span class="text-muted">Queries in Progress</span></h2>
+</body>
+</html>
