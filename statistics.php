1d3977d18faa08163da1fc016082b1b7f3e7ed3c
NewAgeRobot <seanmcdonnell90@gmail.com>
2015-04-07 01:40:51 +0100

    implemented rough version of the interest stats by copying the subject one. Still have to enter all the combinations for the interests. Will do tomorrow.

--- statistics.php
+++ statistics.php
@@ -33,7 +33,7 @@ switch ($subjectAmount){
 	$subject5 = $subjectArray[5];
 	$subject6 = $subjectArray[6];
 
-	$testQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6] FROM subjectFeedback WHERE `Email` = '$userEmail'");
+	$subjectQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6] FROM subjectFeedback WHERE `Email` = '$userEmail'");
 	break;
 
 	case 7: 
@@ -45,7 +45,7 @@ switch ($subjectAmount){
 	$subject6 = $subjectArray[6];
 	$subject7 = $subjectArray[7];
 
-	$testQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7] FROM subjectFeedback WHERE `Email` = '$userEmail'");
+	$subjectQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7] FROM subjectFeedback WHERE `Email` = '$userEmail'");
 	break;
 
 	case 8: 
@@ -58,7 +58,7 @@ switch ($subjectAmount){
 	$subject7 = $subjectArray[7];
 	$subject8 = $subjectArray[8];
 
-	$testQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7], sum($subject8) as $subjectArray[8] FROM subjectFeedback WHERE `Email` = '$userEmail'");
+	$subjectQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7], sum($subject8) as $subjectArray[8] FROM subjectFeedback WHERE `Email` = '$userEmail'");
 	break;
 
 	case 9: 
@@ -72,11 +72,11 @@ switch ($subjectAmount){
 	$subject8 = $subjectArray[8];
 	$subject9 = $subjectArray[9];
 
-	$testQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7], sum($subject8) as $subjectArray[8], sum($subject9) as $subjectArray[9] FROM subjectFeedback WHERE `Email` = '$userEmail'");
+	$subjectQuery = mysql_query("SELECT sum($subject1) as $subjectArray[1], sum($subject2) as $subjectArray[2], sum($subject3) as $subjectArray[3], sum($subject4) as $subjectArray[4], sum($subject5) as $subjectArray[5], sum($subject6) as $subjectArray[6], sum($subject7) as $subjectArray[7], sum($subject8) as $subjectArray[8], sum($subject9) as $subjectArray[9] FROM subjectFeedback WHERE `Email` = '$userEmail'");
 	break;
 }
 
-$listOfTotals = mysql_fetch_assoc($testQuery);
+$subjectTotals = mysql_fetch_assoc($subjectQuery);
 
 
 
@@ -93,12 +93,39 @@ while($row = mysql_fetch_assoc($interestScores)) {
 	foreach ($row as $col => $val) {
 		if ($col == 'ID' || $col == 'Date' || $col == 'Email') continue;
 		if ($val > 0) {
-			echo $col;
-			// $interestArray[] = $col;
+			// echo $col;
+			$interestArray[] = $col;
 		}
 	}
 }
+// $sorted = array_unique($interestArray);
 
+$interestsSorted = array_values(array_unique($interestArray));
+$interestAmount = count($interestsSorted) - 1;
+
+
+switch ($interestAmount){
+	case 3: 
+	$interest1 = $interestsSorted[1];
+	$interest2 = $interestsSorted[2];
+	$interest3 = $interestsSorted[3];
+
+	$interestQuery = mysql_query("SELECT sum($interest1) as $interestsSorted[1], sum($interest2) as $interestsSorted[2], sum($interest3) as $interestsSorted[3] FROM interestFeedback WHERE `Email` = '$userEmail'");
+	break;
+
+	case 4: 
+	$interest1 = $interestsSorted[1];
+	$interest2 = $interestsSorted[2];
+	$interest3 = $interestsSorted[3];
+	$interest4 = $interestsSorted[4];
+
+	$interestQuery = mysql_query("SELECT sum($interest1) as $interestsSorted[1], sum($interest2) as $interestsSorted[2], sum($interest3) as $interestsSorted[3], sum($interest4) as $interestsSorted[4] FROM interestFeedback WHERE `Email` = '$userEmail'");
+	break;
+}
+
+//do out the rest for the possible amoounts they can have - 20 or so? all the interests
+
+$interestTotals = mysql_fetch_assoc($interestQuery);
 
 
 ?>
@@ -120,7 +147,7 @@ data: [//array of dataSeries
 	dataPoints: [
 	<?php
 	for($j = 1; $j <= $subjectAmount; $j++){
-		echo "{ label: '" . $subjectArray[$j] . " - " . $listOfTotals[$subjectArray[$j]] . "', y: " . $listOfTotals[$subjectArray[$j]] . "},";
+		echo "{ label: '" . $subjectArray[$j] . " - " . $subjectTotals[$subjectArray[$j]] . "', y: " . $subjectTotals[$subjectArray[$j]] . "},";
 	} 
 	?>
 	]
@@ -139,8 +166,8 @@ data: [//array of dataSeries
 	type: "column",
 	dataPoints: [
 	<?php
-	for($j = 1; $j <= 12; $j++){
-		echo "{ label: '" . $interestArray[$j] . "', y: 1},";
+	for($o = 1; $o <= $interestAmount; $o++){
+		echo "{ label: '" . $interestsSorted[$o] . " - " . $interestTotals[$interestsSorted[$o]] . "', y: " . $interestTotals[$interestsSorted[$o]] . "},";
 	} 
 	?>
 	]
