c6d614a44dd1a2c71d44a3619a388e15c1631c54
NewAgeRobot <seanmcdonnell90@gmail.com>
2015-05-20 20:30:34 +0100

    added login stat to initial registration login.

--- register.php
+++ register.php
@@ -22,6 +22,7 @@ if ($_POST['register']){
     mysql_query("INSERT INTO `users` (`Username`, `Password`, `Year`, `Email`, `Salt`) VALUES ('$username', '$password', '$year', '$email', '$salt')");
     mysql_query("INSERT INTO `subjectsTable` (`Email`) VALUES ('$email')");
     mysql_query("INSERT INTO `storedInterests` (`Email`) VALUES ('$email')");
+    mysql_query("INSERT INTO `visitStats` (`Email`, `CurrentLogin`) VALUES ('$email', '1')");
     setcookie("c_user", hash("sha512", $email), time() + 12 * 60 * 60, "/");
     setcookie("c_salt", $salt, time() + 12 * 60 * 60, "/");
     header('Location: subjectChoice.php');
