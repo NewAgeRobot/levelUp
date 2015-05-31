58e23f06d4003b78b4516373b8e029a2162ce906
NewAgeRobot <seanmcdonnell90@gmail.com>
2015-05-31 22:14:26 +0100

    finished andreas, doing the others now, added questions font

--- testimonials/andrea.php
+++ testimonials/andrea.php
@@ -37,13 +37,13 @@ $userEmail = $user['Email'];
     });
   </script>
 
- <script type="text/javascript">
-  $(document).ready(function(){
+  <script type="text/javascript">
+    $(document).ready(function(){
       $(".smallMenu .header-image img").click(function() {
         window.location.replace("../homepage.php");
       });
     });
-</script>
+  </script>
 </head>
 
 <body>
@@ -52,7 +52,7 @@ $userEmail = $user['Email'];
 
     <div class="page">
 
-      
+
       <div class="navigationBar">
         <!-- <h1 class="logo"><a href="../homepage.php"><img src="../images/header-logo.png" /></a></h1> -->
 
@@ -81,39 +81,171 @@ $userEmail = $user['Email'];
       </div>
 
 
-    
-    <hr />
 
-    <div class="blog section">
-      <!-- <h1 class="lede"></h1> -->
+      <hr />
 
-      <div class="main">
-        <div class="article">
-          <div class="header">
-            <h1 class="title">Select which story you'd like to hear</h1>
-          </div><!-- /end .header -->
+      <div class="blog section">
+        <!-- <h1 class="lede"></h1> -->
 
-          <div class="intro">
-          <div class="centeredIntro">
+        <div class="main">
+          <div class="article">
+            <div class="header">
+              <h1 class="title"></h1>
+            </div><!-- /end .header -->
+
+            <div class="intro">
+              <div class="testimoneyTopBox">
+                <img src="../images/people/Andrea_Thumbnail.png">
+                <div class="testimonyName">Andrea McConnon</div>
+                <div class="testimonyTitle">Electrical Engineer</div>
+                <!-- <div class="titleRibbon">Apprenticeship</div> -->
+              </div>
+              
+              <div class="WW">
+                <table>
+                  <tr>
+                    <td>
+                      What:
+                    </td>
+                    <td>
+                      Apprenticeship
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      Where
+                    </td>
+                    <td>
+                      ESB Networks
+                    </td>
+                  </tr>
+                </table>
+              </div>
+                <div class="questionsBlock">When you were finishing secondary school did
+                  you always know what you wanted to do afterwards?</div>
+                  <div class="lightAnswerBlock">No, definitely not. I'd always said I wanted to be a PE teacher, and
+                    it wasn't until the day that I got my leaving cert results and got
+                    the points for it that I realised I definitely didn't want it!</div>
+                    <div class="questionsBlock">Is what you are doing now something
+                      you always wanted to do?</div>
+                      <div class="darkAnswerBlock">It was something I'd always thought about doing because I wanted
+                        to do something practical or hands-on, but I never thought I'd
+                        enjoy it as much as I do.</div>
+                        <div class="questionsBlock">What made you decide to do an apprenticeship?</div>
+                        <div class="lightAnswerBlock">When the career guidance teacher came around to my class to
+                          mainly tell the lads that the ESB were recruiting, I contemplated
+                          applying. When I was filling out the application form I never
+                          intended to take the job, it was always just a back-up plan if
+                          everything went wrong in the leaving cert. And even though
+                          everything went much better than expected in the exams, and I
+                          got my first choice on my CAO form, I realised I didn't want what
+                          I'd been offered. So I decided to start the apprenticeship with the
+                          intentions of using it as a gap year and a chance to earn some
+                          money, and had planned on re-applying to the CAO the following
+                          year. However, the ESB were offering their apprentices the chance
+                          to go to college to study electrical engineering after completing
+                          the first 2 years of the apprenticeship. They fully fund this by
+                          paying your college fees and giving you enough money to cover all
+                          of your expenses (accommodation, travel, food, etc.). This is the
+                          road I went down, but the option was also there to finish out the
+                          apprenticeship and become a fully qualified electrician.</div>
+                          <div class="questionsBlock">Where did you undertake this apprenticeship?</div>
+                          <div class="darkAnswerBlock">I did my apprenticeship with ESB Networks.</div>
+                          <div class="questionsBlock">How did you find the experience?</div>
+                          <div class="lightAnswerBlock">I've really enjoyed all of it. The apprenticeship is great because
+                            you learn while you work, you get to earn money so you get some
+                            independence, and you don't spend too long in college at any one
+                            time. Because ESB Networks is split into different areas of work, as
+                            an apprentice, you get to move around all of the different areas
+                            every 3 or 4 months, so if you don't like one area you only have to
+                            do it for a few months and then you can move on.
+                            The ordinary level degree I completed was Electrical and Control
+                            Engineering in DIT (DT009). This course was great because a lot of
+                            time was spent in labs doing practical work like building and
+                            programming robots to fight each other in robot wars.
+                            I'm currently completing the honours degree in electrical/
+                            electronic engineering in DIT (DT021), and have one year left.
+                            Because I had done two years of the apprenticeship with the ESB, I
+                            got to go straight into 2nd year of the ordinary degree, and then
+                            following that, I went into 3rd year of the honours degree.</div>
+                            <div class="questionsBlock">How has it benefited you personally?</div>
+                            <div class="darkAnswerBlock">By doing the apprenticeship first, I felt that it gave me a chance to
+                              mature and really think about what I wanted to do. So when I did
+                              go to college to do the engineering degree I was really ready to
+                              start studying. Whereas if I'd gone straight to college after school I
+                              think I would have been burned out after all the studying for the
+                              leaving cert to give college my all.</div>
+                              <div class="questionsBlock">Did the apprenticeship involve any
+                                course work or further study?</div>
+                                <div class="lightAnswerBlock">The apprenticeship itself consists of 7 phases spread out over 4
+                                  years. Phases 1, 3, 5 & 7 are on the job, and phases 2, 4 & 6 are in
+                                  college. Phase 2 lasts 20 weeks, and takes place in FAS/SOLAS
+                                  training centres. This phase is split 50/50 between theory and
+                                  practical work. Phases 4 & 6 both last 10 weeks and take place in
+                                  ITs. These two phases are much more theory based, with one day a
+                                  week spent in the workshop. Because I was going to study electrical
+                                  engineering after the 2nd year, I had to complete engineering
+                                  bridging modules in DIT before going into 2nd year of the degree.
+                                  This was 6 weeks long and was completed in two 3 week blocks
+                                  before and after completing Phase 4 of the apprenticeship.</div>
+                                  <div class="questionsBlock">Have you always thought it was
+                                    important to work at something you love?</div>
+                                    <div class="darkAnswerBlock">Definitely, if you enjoy what you're doing then you'll never dread
+                                      getting up for work in the morning.</div>
+                                      <div class="questionsBlock">What advice would you give to current leaving cert
+                                        students deciding on college courses/career paths?</div>
+                                        <div class="bottomAnswerBlock">Whether you know what you want to do or not, aim to get the
+                                          highest points you possibly can. That way if like me you decide
+                                          you don't want to do what you thought you wanted to do, you'll
+                                          have left yourself with as many opportunities as you possibly can.
+                                          Don't panic if you don't know what you want do to, most people
+                                          don't. Make sure you give yourself some back-up options whether
+                                          it be an apprenticeship or a PLC course.
+                                          Don't rely solely on your CAO application. If you can, try to get a
+                                          weeks work experience in whatever it is you think you might want
+                                          to do. Do it the summer after your leaving cert even if it is after
+                                          the CAO deadline.
+                                          It's better to know then if you don't want to do it than to spend a
+                                          year in college to discover it. I got a weeks work experience in a
+                                          soccer summer camp, and it was then that I realised I didn't really
+                                          want to be a PE teacher. Whatever it is that you want to do, it's
+                                          not out of your reach. There are back ways into everything. So
+                                          don't let the points dictate what career you choose.</div>
+
+              <!-- 
+              <div class="questionsBlock">When</div>
+              <div class="lightAnswerBlock">whn</div>
+              <div class="questionsBlock">When</div>
+              <div class="darkAnswerBlock">sfdfs</div>
+              <div class="questionsBlock">When</div>
+              <div class="lightAnswerBlock">whn</div>
+              <div class="questionsBlock">When</div>
+              <div class="darkAnswerBlock">sfdfs</div>
+              <div class="questionsBlock">When</div>
+              <div class="lightAnswerBlock">whn</div>
+              <div class="questionsBlock">When</div>
+              <div class="darkAnswerBlock">sfdfs</div>
+              <div class="questionsBlock">When</div>
+              <div class="lightAnswerBlock">whn</div>
+              <div class="questionsBlock">When</div>
+              <div class="darkAnswerBlock">sfdfs</div>
+              <div class="questionsBlock">When</div>
+              <div class="lightAnswerBlock">whn</div>
+               -->
 
 
-fgdfgdfg
+                                          <!-- <div class="meta section">
 
+                                          </div> <!-- /end .meta.section --> 
+                                        </div><!-- /end .article -->
+                                      </div><!-- /end .main -->
+                                    </div><!-- /end .blog.section -->
 
+                                    <div id="footer">
+                                    </div><!-- /end #footer -->
+                                  </div><!-- /end .page -->
 
-           <div class="meta section">
-            
-           </div> <!-- /end .meta.section -->
-           </div>
-         </div><!-- /end .article -->
-       </div><!-- /end .main -->
-     </div><!-- /end .blog.section -->
+                                </div><!-- /end .site -->
 
-     <div id="footer">
-    </div><!-- /end #footer -->
-  </div><!-- /end .page -->
-
-</div><!-- /end .site -->
-
-</body>
-</html>
+                              </body>
+                              </html>
