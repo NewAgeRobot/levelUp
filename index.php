0990956ad5b6f77afdf6ed96a5f1843335e5d8fa
NewAgeRobot <seanmcdonnell90@gmail.com>
2015-06-02 04:09:31 +0100

    replaced index with test

--- index.php
+++ index.php
@@ -1,149 +1,166 @@
-<?php
-include "connect.php";
-include "algor.php";
-if($logged == true){
-  header('Location: homepage.php');
-}
-?>
-<!DOCTYPE html>
 <html lang="en">
 <head>
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Level Up</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+  <meta charset="utf-8" />
+  <title>LevelUp</title>
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="LevelUp" content="Level Up - http://www.Levelup.ie" />
+  <meta name="description" content="A website to help students better discover what they enjoy and want to pursue after second level." /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/robotCss.css" media="screen, projection" />
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/robotCss.css">
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-  <link rel="stylesheet" href="css/custom.css">
+  <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+  <link rel="stylesheet" href="css/jquery-mobile.css" />
+  <script src="js/jquery-mobile.js"></script>
+  <link rel="stylesheet" href="css/dropit.css" />
 <!-- bxSlider CSS file -->
 <link href="css/jquery.bxslider.css" rel="stylesheet" />
+  <script src="js/dropit.js"></script>
+  <script src="js/redirect.js"></script>
+  <link rel="stylesheet" href="css/skeleton.css">
 
-  <!-- Scripts
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('.menu').dropit();
+    });
+  </script>
+
+ <script type="text/javascript">
+  $(document).ready(function(){
+      $(".smallMenu .header-image img").click(function() {
+        window.location.replace("homepage.php");
+      });
+    });
+</script>
 <!-- bxSlider Javascript file -->
 <script src="js/jquery.bxslider.js"></script>
 <script type="text/javascript">
   $(document).ready(function(){
-    $('.bxslider').bxSlider();
+    $('.bxslider').bxSlider({
+      auto: true
+    });
   });
 </script>
 </head>
-<body>
 
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+<body style="background:#fff;">
 
+  <div class="site">
+    <div class='whiteHeader'><span class="currentPageSpanLeft"><a href="homepage.php" data-ajax='false'><img src="images/icons/Home_Icon.png"></a></span><span class="currentPageSpanRight"><a href="#team">Meet the Team</a></span></div>
 
+    <div class="page">
 
-  <div id="navigationBar">
-      <div id="logo"><a href="index.php"><img src="images/icons/LOGO_withFont.png"></a></div>
-    </div>
-
-  <div class="section hero">
-    <div class="container">
-      <div class="row">
-        <div class="one-half column">
-          <h4 class="hero-heading">Your future, in your hands.</h4>
-          <a class="button button-primary" href="register.php">Register</a>
-          <a class="button button-primary" href="login.php">Log In</a>
-        </div>
-        <div class="one-half column phones">
-          <img class="phone" src="images/iphone-new.png">
-          <img class="phone" src="images/iphone-new.png">
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="section values">
-    <div class="container">
-
-      <ul class="bxslider">
-        <li><img src="images/slideshow/WebsiteStat01.jpg" width="100%" /></li>
-        <li><img src="images/slideshow/WebsiteStat02.jpg" width="100%" /></li>
-        <li><img src="images/slideshow/WebsiteStat03.jpg" width="100%" /></li>
-        <li><img src="images/slideshow/WebsiteStat04.png" width="100%" /></li>
-        <li><img src="images/slideshow/WebsiteStat05.png" width="100%" /></li>
-      </ul>
-
-    </div>
-  </div>
-
-  <div class="section get-help">
-    <div class="container">
-      <h3 class="section-heading">Need help getting started?</h3>
-      <p class="section-description">Here at LevelUp we believe that by simply including your interests and hobbies in your search for courses, you will surprise yourself with the wide range of areas of study available in Ireland.
-As well as providing a new and engaging way to search for college courses, we also give information on the many other career and education paths available after school, information from professionals who have been through the experience, from PLC courses to apprenticeships to entrepreneurs.</p>
-      <a class="button button-primary" href="register.php">Register</a>
-    </div>
-  </div>
-
-  <div class="section categories">
-    <div class="container">
-      <h3 class="section-heading">Come see us at FÍS!</h3>
-      <p class="section-description"></p>
-      <div class="row">
-      <div class="videoWrapper">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/RfM24d1hboE" frameborder="0" allowfullscreen></iframe>
-      </div>
       
-        <!-- <div class="column category">
-        </div> -->
+      <div class="navigationBar">
+        <!-- <h1 class="logo"><a href="homepage.php"><img src="images/header-logo.png" /></a></h1> -->
+
+        <ul class="nav nav-primary bigMenu">
+          <div class="logoTest"><li id="logoImage"><a href="homepage.php" data-ajax='false'><img src="images/icons/LOGO_withFont.png"></a></li></div>
+          <div style="visibility:hidden;"><li id="nav-explore" class="first"><a href="exploreInterests.php" data-ajax='false'>&nbsp;&nbsp;Explore&nbsp;&nbsp;&nbsp;</a></li><li id="nav-feedback" class="second"><a href="interestFeedback.php" data-ajax='false'>|&nbsp;&nbsp;Rate&nbsp;&nbsp;</a></li><li id="nav-stats" class="third"><a href="statistics.php" data-ajax='false'>|&nbsp;&nbsp;Statistics&nbsp;&nbsp;</a></li><li id="nav-test" class="fourth"><a href="testimonials.php" data-ajax='false'>|&nbsp;&nbsp;Testimonials&nbsp;&nbsp;</a></li><li id="nav-saved" class="fifth"><a href="savedCourses.php" data-ajax='false'>|&nbsp;&nbsp;Saved Courses&nbsp;&nbsp;</a></li><li id="nav-log" class="sixth"><a href="logout.php" data-ajax='false'>|&nbsp;&nbsp;Log Out&nbsp;&nbsp;</a></li>
+       </div> </ul><!-- /end ul#nav-primary.nav -->
+
+
+       <ul class="nav nav-primary smallMenu">
+          <li class="header-image"><img src="images/icons/LOGO_ForMobile.png"></li>
+         <div style="visibility:hidden;"> <ul class="menu">
+            <li>
+              <a href="#">&#9776; Menu</a>
+              <ul>
+                <li><a href="exploreInterests.php" data-ajax='false'><img src="images/icons/icon_ExploreCourses.jpg">Explore</a></li>
+                <li><a href="savedCourses.php" data-ajax='false'><img src="images/icons/icon_SavedCourses.jpg">Saved Courses</a></li>
+                <li><a href="interestFeedback.php" data-ajax='false'><img src="images/icons/icon_MyRecords.jpg">Rate</a></li>
+                <li><a href="statistics.php" data-ajax='false'><img src="images/icons/icon_Statistics.jpg">Statistics</a></li>
+                <li><a href="testimonials.php" data-ajax='false'><img src="images/icons/icon_Testimonials.jpg">Testimonials</a></li>
+                <li><a href="logout.php" data-ajax='false'><img src="images/icons/icon_LogOut.jpg">Log Out</a></li>
+              </ul>
+            </li>
+          </ul></div>
+        </ul><!-- /end ul#nav-primary.nav -->
       </div>
-    </div>
-  </div>
-
-  <div class="section get-help">
-    <div class="container">
-      <h3 class="section-heading">The Team</h3>
-      <div class="row">
-        <div class="three columns">
-        <img class="bioImages" src="images/Sinead.jpg">
-          <h5 class="value-heading">Sinéad</h5>
-          <h6 class="value-heading">Project Lead</h6>
-          <p class="value-description">Sinéad Duffy is a freelance Graphic Designer and Animator. Recent work includes re-branding of the DkIT Music Department and various eLearning projects for educational publishers including Gill & Macmillan and Folens.</p>
-        </div>
-        <div class="three columns">
-        <img class="bioImages" src="images/Pauric.jpg">
-          <h5 class="value-heading">Pauric</h5>
-          <h6 class="value-heading">Content Developer</h6>
-          <p class="value-description">Pauric Martin is a graduate of Communications in Creative Multimedia. Having studied in a wide variety of areas such as design, audio and video, he has used these to create high quality projects both individually and as part of a group.</p>
-        </div>
-        <div class="three columns">
-        <img class="bioImages" src="images/Sian.jpg">
-          <h5 class="value-heading">Sían</h5>
-          <h6 class="value-heading">Designer</h6>
-          <p class="value-description">Sían is a freelance graphic designer with a Degree in Communications in Creative Multimedia. Sían loves the use of colour in clean and contemporary web design.</p>
-        </div>
-        <div class="three columns">
-        <img class="bioImages" src="images/Sean.jpg">
-          <h5 class="value-heading">Seán</h5>
-          <h6 class="value-heading">Programmer</h6>
-          <p class="value-description">Seán McDonnell is a freelance web developer with a Degree in Communications in Creative Multimedia. He has worked as lead programmer on several commercial products and enjoys new challenges.</p>
-        </div>
-      </div>
-    </div>
-  </div>
 
 
+    
+    <hr />
 
+    <div class="blog section" style="background:#fff;">
+      <!-- <h1 class="lede"></h1> -->
 
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+      <div class="main">
+        <div class="article">
+          <div class="header">
+            <h1 class="title"></h1>
+          </div><!-- /end .header -->
+
+          <div class="intro" style="background:#fff;">
+          <div class="blurb" style="padding:25px;color:#1b1b27;">
+            Here at LevelUp we provide a new and engaging way to search for college courses, we also give information on the many other career and education paths available after school, as well as information from professionals who have been through the experience - from PLC courses to apprenticeships to entrepreneurs.
+<table class='saveNextButtons savedCoursesButtons'><tr><td>
+                <a href='login.php' class='backArrow' data-ajax='false'><img src='images/icons/logIn_btn.png'></a>
+                </td><td>
+                <a href='register.php' class='backArrow' data-ajax='false'><img src='images/icons/logIn_btn.png'></a>
+                </td></tr></table>
+          </div>
+          <div class="slideshow">
+            <ul class="bxslider">
+              <li><img src="images/slideshow/WebsiteStat01.jpg" width="100%" /></li>
+              <li><img src="images/slideshow/WebsiteStat02.jpg" width="100%" /></li>
+              <li><img src="images/slideshow/WebsiteStat03.jpg" width="100%" /></li>
+              <li><img src="images/slideshow/WebsiteStat04.png" width="100%" /></li>
+              <li><img src="images/slideshow/WebsiteStat05.png" width="100%" /></li>
+            </ul>
+          </div>
+          
+              <div class="meta section" style="background:#1b1b27;">
+              <div class="videoWrapper">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/RfM24d1hboE" frameborder="0" allowfullscreen></iframe>
+              </div>
+            </div> <!-- /end .meta.section -->
+
+              <div class="meta section" style="background:#fff;">
+              <a name="team"></a>
+              <div class="row">
+                <div class="three columns">
+                <img class="bioImages" src="images/Sinead.jpg">
+                  <h5 class="value-heading">Sinéad</h5>
+                  <h6 class="value-heading">Project Lead</h6>
+                  <p class="value-description">Sinéad Duffy is a freelance Graphic Designer and Animator. Recent work includes re-branding of the DkIT Music Department and various eLearning projects for educational publishers including Gill & Macmillan and Folens.</p>
+                </div>
+                <div class="three columns">
+                <img class="bioImages" src="images/Pauric.jpg">
+                  <h5 class="value-heading">Pauric</h5>
+                  <h6 class="value-heading">Content Developer</h6>
+                  <p class="value-description">Pauric Martin is a graduate of Communications in Creative Multimedia. Having studied in a wide variety of areas such as design, audio and video, he has used these to create high quality projects both individually and as part of a group.</p>
+                </div>
+                <div class="three columns">
+                <img class="bioImages" src="images/Sian.jpg">
+                  <h5 class="value-heading">Sían</h5>
+                  <h6 class="value-heading">Designer</h6>
+                  <p class="value-description">Sían is a freelance graphic designer with a Degree in Communications in Creative Multimedia. Sían loves the use of colour in clean and contemporary web design.</p>
+                </div>
+                <div class="three columns">
+                <img class="bioImages" src="images/Sean.jpg">
+                  <h5 class="value-heading">Seán</h5>
+                  <h6 class="value-heading">Programmer</h6>
+                  <p class="value-description">Seán McDonnell is a freelance web developer with a Degree in Communications in Creative Multimedia. He has worked as lead programmer on several commercial products and enjoys new challenges.</p>
+                </div>
+              </div>
+            </div> <!-- /end .meta.section -->
+          </div><!-- /end .article -->
+        </div><!-- /end .main -->
+      </div><!-- /end .blog.section -->
+
+      <div id="footer">
+      <!-- <p>Images &copy; their respective copyright holders.</p>
+
+      <p>The design and code is &copy; 2014 <a href="http://unstoppablerobotninja.com/">Ethan Marcotte</a>, supporting his book <cite><a href="http://www.abookapart.com/products/responsive-web-design">Responsive Web Design</a></cite>.</p>
+
+      <p>Beep boop beep.</p> -->
+    </div><!-- /end #footer -->
+  </div><!-- /end .page -->
+
+</div><!-- /end .site -->
+
 </body>
 </html>
